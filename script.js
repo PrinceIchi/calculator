70bfbfd90b977deeffa755ebc6855f004fb36561
Prince <principe3219@icloud.com>
2024-12-29 16:38:36 -0800

    more practice

---
+++ script.js
@@ -0,0 +1,9 @@
+const resultText = document.getElementsByClassName("result-text")[0];
+
+function onNumberPressed(number) {
+    if (number === ".") {
+        if (resultText.innerHTML.length === 0 && resultText.innerHTML.includes(".")) return;
+    }
+
+    resultText.innerHTML += number.toString();
+}
